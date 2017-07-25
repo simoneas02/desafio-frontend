4dc7e515ab286abb2382bcf433a71a9a3f7619fa
simoneas02 <simoneas02@hotmail.com>
2017-07-25 18:14:54 -0300

    first scaffolding

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Previsão do Tempo</h1>
+        <input type='search' placeholder='Insira aqui o nome da cidade' />
+      </div>
+    );
+  }
+}
+
+export default App;
